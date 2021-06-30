f8e830345075a39f97a4200a774100a08d109a42
Ahmad zaky <53550696+saveitlol@users.noreply.github.com>
2021-06-30 14:22:32 +0700

    Update rss.py

--- rss.py
+++ rss.py
@@ -33,7 +33,7 @@ def check_feed():
     entry = FEED.entries[0]
     if entry.id != db.get_link(feed_url).link:
                    # ↓ Edit this message as your needs.
-      message = f"/rss {entry.link}"
+      message = f"/leech {entry.link}"
       try:
         app.send_message(log_channel, message)
         db.update_link(feed_url, entry.id)
